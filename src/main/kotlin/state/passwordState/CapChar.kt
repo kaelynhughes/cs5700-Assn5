11f8ff8b1de8c31f33f8f2fa57054eccd04b0209
Kaelyn Hughes <kaelynmhughes@gmail.com>
2024-08-08 22:19:49 -0500

    add invalid arg handling for all password states that didnt already have it

--- src/main/kotlin/state/passwordState/CapChar.kt
+++ src/main/kotlin/state/passwordState/CapChar.kt
@@ -2,10 +2,16 @@ package org.example.state.passwordState
 
 class CapChar: PasswordState() {
     override fun nextCharacter(char: String): PasswordState {
-        TODO("Not yet implemented")
+        require (char.length == 1) {
+            "This function takes exactly one character."
+        }
+        TODO()
     }
 
     override fun nextCharLong(char: String): PasswordState {
+        require (char.length == 1) {
+            "This function takes exactly one character."
+        }
         TODO("Not yet implemented")
     }
 }
--- src/main/kotlin/state/passwordState/PasswordLong.kt
+++ src/main/kotlin/state/passwordState/PasswordLong.kt
@@ -2,9 +2,15 @@ package org.example.state.passwordState
 
 class PasswordLong: PasswordState() {
     override fun nextCharacter(char: String): PasswordState {
+        require (char.length == 1) {
+            "This function takes exactly one character."
+        }
         TODO("Not yet implemented")
     }
     override fun nextCharLong(char: String): PasswordState {
+        require (char.length == 1) {
+            "This function takes exactly one character."
+        }
         TODO("Not yet implemented")
     }
 }
